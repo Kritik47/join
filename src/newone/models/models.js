2bc7ef9307f53b706f2ad88855e137f5865214b6
KRITIK KUMAR <Kritiksahu958958@gmail.com>
2024-05-21 11:46:31 +0530

    Update

--- src/newone/models/models.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const UserSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String,
-    reenterPassword:String
-})
-const user=mongoose.model('user',UserSchema);
-module.exports=user;
